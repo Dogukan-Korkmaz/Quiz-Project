4269bb93fad49d53e7f7620374fb738f5262b2f0
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-01-28 20:55:16 +0300

    bugfix and imported a 30 question list

--- quiz_brain.py
+++ quiz_brain.py
@@ -7,15 +7,15 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): .").capitalize()
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ").capitalize()
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer == correct_answer:
-            print(f"Your current score is :{self.score_up_or_down(True)}/{self.question_number}.")
+            print(f"Your current score is: {self.score_up_or_down(True)}/{self.question_number}.")
             print("The answer is correct !")
         else:
-            print(f"Your current score is :{self.score_up_or_down(False)}/{self.question_number}.")
+            print(f"Your current score is: {self.score_up_or_down(False)}/{self.question_number}.")
             print(f"Correct answer was {correct_answer}.")
 
     def score_up_or_down(self, con):
@@ -25,12 +25,11 @@ class QuizBrain:
             self.score -= 1
         return self.score
 
-
     def still_has_questions(self):
         if len(self.question_list) > self.question_number:
             print(f"Here comes the {self.question_number + 1}. question !")
             return True
         else:
             print("Game is finished !")
-            print(f"Your final score is {self.score_up_or_down(None)}.")
+            print(f"Your final score is {self.score}.")
             return False
